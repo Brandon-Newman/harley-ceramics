a7f3845ed13c3f8ceeb544d441a7e23bc5f7617b
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2023-08-15 13:38:38 -0500

    built routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/catalog", to: "chassis#list_catalog"
+
+  get "/motorcycle-chassis", to: "chassis#list_chassis"
+
+  get "/models-by-chassis", to: "model#list_models"
+
+  get "/colors-by-model", to: "model#list_model_colors"
+
+  get "/paint-colors", to: "color#list_all_colors"
+
+  post "/new-chassis", to: "chassis#new_chassis"
+
+  post "/new-model", to: "model#new_model"
+
+  post "/new-color", to: "color#new_color"
+
+  patch "/add-color-to-model", to: "model#add_color_to_model"
+
+  delete "/chassis", to: "chassis#delete_chassis"
+
+  delete "/model", to: "model#delete_model"
+
+  delete "/color", to: "color#delete_color"
+  
 end
