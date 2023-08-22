3ca9c09e927e55abb97c12ccf5f98fccf8f5022a
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2023-08-22 08:02:11 -0500

    can create new models

--- app/controllers/model_controller.rb
+++ app/controllers/model_controller.rb
@@ -15,22 +15,37 @@ class ModelController < ApplicationController
 
     def list_model_colors
         model = Model.find(params[:id])
-        # color_ids = ModelColor.where(model_id: model.id).pluck('color_id')
-        # color_desc = color_ids.map { |id| Color.find(id).description}
         color_desc = ModelColor.where(model_id: model.id).pluck('color_id').map { |id| Color.find(id).description}
 
         render json: {id: model.id, model: model.name, colors: color_desc}, status: :ok
     end
 
     def new_model
+        is_new_bike = Model.where(name: params[:model_name]).empty?
+        return render json: "Model already exists", status: :bad_request unless is_new_bike
+        model = Model.create(chassi_id: params[:chassi_id], name: params[:model_name])
+        model_id = model.id
+        available_colors = []
+
+        params[:colors].each do |color|
+            ModelColor.create(model_id: model_id, color_id: color)
+            available_colors.push(Color.find(color).description)
+        end
+
+        new_bike = {
+            chassis: Chassi.find(params[:chassi_id]).style,
+            model_name: model.name,
+            available_colors: available_colors
+        }
+        render json: new_bike, status: :ok
         #need join table before creating models 
     end
 
     def add_color_to_model
         model = Model.find(params[:id])
         color = Color.find(params[:color_number])
-        new_combination = ModelColor.where(model_id: model.id, color_id: color.id).empty?
-        return render json: "Combination already exists", status: :bad_request unless new_combination
+        is_new_combination = ModelColor.where(model_id: model.id, color_id: color.id).empty?
+        return render json: "Combination already exists", status: :bad_request unless is_new_combination
         ModelColor.create(model_id: model.id, color_id: color.id)
         render json: "#{model.name} now available in #{color.description}", status: :ok
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,31 @@
 Rails.application.routes.draw do
 
-  get "/catalog", to: "chassis#list_catalog" #done
+  get "/catalog", to: "chassis#list_catalog" 
 
-  get "/motorcycle-chassis", to: "chassis#list_chassis" #done
+  get "/motorcycle-chassis", to: "chassis#list_chassis" 
 
-  get "/models-by-chassis/:id", to: "model#list_models" #done
+  get "/models-by-chassis/:id", to: "model#list_models" 
 
-  get "/list-model-colors/:id", to: "model#list_model_colors" #done
+  get "/list-model-colors/:id", to: "model#list_model_colors" 
 
-  get "/paint-colors", to: "color#list_all_colors" #done
+  get "/paint-colors", to: "color#list_all_colors" 
 
-  post "/new-chassis", to: "chassis#new_chassis" #done
+  post "/new-chassis", to: "chassis#new_chassis" 
 
   post "/new-model", to: "model#new_model"
 
-  post "/new-color", to: "color#new_color" #done
+  post "/new-color", to: "color#new_color" 
 
-  patch "/add-color-to-model/:id", to: "model#add_color_to_model" #done
+  patch "/add-color-to-model/:id", to: "model#add_color_to_model" 
 
-  delete "/delete-chassis/:id", to: "chassis#delete_chassis"
+  delete "/delete-chassis/:id", to: "chassis#delete_chassis" #to_be_done
 
-  delete "/delete-model/:id", to: "model#delete_model"
+  delete "/delete-model/:id", to: "model#delete_model" #to_be_done
 
-  delete "/delete-color/:id", to: "color#delete_color"
+  delete "/delete-color/:id", to: "color#delete_color" #to_be_done
 
   # Special Project
-  post "/create-custom-bike", to: "controller#action"
+  post "/create-custom-bike", to: "controller#action" #to_be_done
   #create a customer "new bike model" by choosing a current chassis, model, and color. Color can be existing or a new custom color
   
 end
